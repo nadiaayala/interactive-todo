7614f211cd595c2411d82cc339b1e12f1ffe94d7
Nadia <nadiaayalanogueira@outlook.com>
2019-05-13 20:02:58 +0400

    Separate file in folders

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Nadia Ayala on 13/05/19.
+//  Copyright © 2019 Nadia Ayala. All rights reserved.
+//
+
+import Foundation
+
+class item {
+    var title: String = ""
+    var done: Bool = false
+}
